2c4fddc1a0b79e46eba640af566e82e9854ba5ba
Evan Cope <copev313@gmail.com>
2021-01-20 09:32:27 -0500

    added magic 8 ball

---
+++ magic_8_ball.py
@@ -0,0 +1,29 @@
+# magic_8_ball.py
+
+# Give the all knowing ball a shake
+
+import random
+import time
+
+print("###  MAGIC 8 BALL  ###")
+
+def shake():
+    # Generate random num btw 0 & 8.
+    r = random.randint(0, 8)
+    print(" . . . Shaking . . . ")
+    # List of possible fortunes.        
+    fortune_list = ["It is certain",
+                    "It is decidedly so",
+                    "Yes",
+                    "Reply hazy try again",
+                    "Ask again later",
+                    "Concentrate and try again",
+                    "My reply is no",
+                    "Outlook not so good",
+                    "Very doubtful"             ]
+    
+    time.sleep(5)               # Add some suspense
+    return fortune_list[r]      # Return reply
+    
+    
+print( shake() )
